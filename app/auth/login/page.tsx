7f6a3a142c5abcff1c2c1f3ade349fa735615532
Adminkaran <khamunepali786@gmail.com>
2026-06-04 21:28:02 +0545

    this is the first login page for the auth

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <h2> THis is first attempt for using the login. </h2>
+      {/* Add your login form here */}
+    </div>
+  );
+}
