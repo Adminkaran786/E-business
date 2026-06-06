9410253c996a312737fa292d607b5d5b724a59e3
Adminkaran <khamunepali786@gmail.com>
2026-06-06 06:12:19 +0545

    implement in the product file.

--- app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function LoginPage() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-      <h2> THis is first attempt for using the login. </h2>
-      {/* Add your login form here */}
-    </div>
-  );
-}
---
+++ app/products/[id]/page.js
@@ -0,0 +1,8 @@
+export default function ProductPage({ params }) {
+  return (
+    <div>
+      <h1>Product {params.id}</h1>
+      <h2>Product details will go here.</h2>
+    </div>
+  );
+}   
